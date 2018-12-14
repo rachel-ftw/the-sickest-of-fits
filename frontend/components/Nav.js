7a967830fd25c92151c88ca34506092c6f28abf4
Rachel Ralston <rachelralston@gmail.com>
2018-12-14 08:42:36 -0800

    FE: added loading bar, fixed nav, added initial styles from server

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,11 +4,20 @@ import NavStyles from './styles/NavStyles'
 
 const Nav = () => (
   <NavStyles>
-    <Link href="/">
-      <a>home</a>
+    <Link href="/items">
+      <a>Items</a>
     </Link>
     <Link href='/sell'>
       <a>Sell</a>
+    </Link>
+    <Link href='/signup'>
+      <a>Signup</a>
+    </Link>
+    <Link href='/orders'>
+      <a>Orders</a>
+    </Link>
+    <Link href='/me'>
+      <a>Account</a>
     </Link>
   </NavStyles>
 )
