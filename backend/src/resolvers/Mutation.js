3cec78555bf860ee64aa51aa10a3664352421273
Rachel Ralston <rachelralston@gmail.com>
2018-12-15 16:39:38 -0800

    BE: created first item query/mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if they're logged in
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info)
+
+    return item
+  }
+}
 
 module.exports = Mutations;
