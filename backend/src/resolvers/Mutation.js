82b968eb2309578b68d9ad4e5d75d6f71d9b1ebf
Rachel Ralston <rachelralston@gmail.com>
2018-12-17 15:17:47 -0800

    BE: udpated queries/mutations for retrieve and update 1 item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -4,6 +4,16 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info)
 
     return item
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = {...args}
+    delete updates.id
+    return ctx.db.mutation.updateItem({
+        data: updates,
+        where: { id: args.id }
+      },
+      info
+    )
   }
 }
 
