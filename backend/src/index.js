043922f4c6da4b397c3f2ba01b85467b9318e2a1
Rachel Ralston <rachelralston@gmail.com>
2018-12-20 22:31:44 -0500

    BE: finished user signUp data flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
+const cookieParser = require('cookie-parser')
 require('dotenv').config({ path: 'variables.env'})
 
 const createServer = require('./createServer')
 const server = createServer()
 
-// TODO use express middleware to handle cookies/JWT
+server.express.use(cookieParser())
+
 // TODO use express middleware to populate current user
 
 server.start({
@@ -11,5 +13,7 @@ server.start({
     credentials: true,
     origin: process.env.FRONTEND_URL
   }},
-  details => {console.log(`Server running 🏋️‍♀️  💅 || http://localhost:${details.port}`)}
+  details => {
+    console.log(`Server running 🏋️‍♀️  💅 || http://localhost:${details.port}`)
+  }
 )
