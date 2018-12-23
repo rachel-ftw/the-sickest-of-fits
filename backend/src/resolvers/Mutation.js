d006d670781172db957a4b2dca7deb69ff4e14d2
Rachel Ralston <rachelralston@gmail.com>
2018-12-23 14:38:04 -0500

    BE: added User relationship to createItem mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -13,8 +13,18 @@ const signJWTAndCreateCookie = (user, ctx) => {
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO check if they're logged in
-    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info)
+    if (!ctx.request.userId) throw new Error('You must be logged in to do that.')
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        user: {
+          connect: {
+            id: ctx.request.userId,
+          },
+        },
+        ...args
+      }
+    }, info)
 
     return item
   },
