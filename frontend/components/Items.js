5ad35a5c156569873561bebb436aa6c1246b92dc
Rachel Ralston <rachelralston@gmail.com>
2018-12-23 13:14:11 -0500

    minor formatting refactor

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -5,7 +5,7 @@ import { Query } from 'react-apollo'
 
 import Item from './Item'
 import Pagination from './Pagination'
-import { perPage } from '../config';
+import { perPage } from '../config'
 
 export const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}, ) {
--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,7 +1,6 @@
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import PropTypes from 'prop-types'
-import { userInfo } from 'os';
 
 export const CURRENT_USER_QUERY = gql`
   query {
