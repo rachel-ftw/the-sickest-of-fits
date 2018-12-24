fe0da46ab5267932b589fc21847845884740c7b2
Rachel Ralston <rachelralston@gmail.com>
2018-12-24 15:16:58 -0500

    FE added apollo app state & modified to pull cartOpen from there

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,8 @@
 import withApollo from 'next-with-apollo'
 import ApolloClient from 'apollo-boost'
+
 import { endpoint } from '../config'
+import { LOCAL_STATE_QUERY } from '../components/Cart'
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -10,6 +12,22 @@ function createClient({ headers }) {
         fetchOptions: { credentials: 'include' },
         headers,
       })
+    },
+    clientState: { // client side aplicaiton state
+      defaults: { cartOpen: true },
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            const { cartOpen } = cache.readQuery({ query: LOCAL_STATE_QUERY})
+            console.log(cartOpen)
+            const data = {
+              data: { cartOpen: !cartOpen }
+            }
+            cache.writeData(data)
+            return data
+          },
+        },
+      },
     },
   })
 }
